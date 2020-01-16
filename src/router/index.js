e6a06002b600bb68204f9d53cc52876cd3400fce
nargok <nargok@gmail.com>
2020-01-17 00:12:46 +0900

    create text page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Texts from '../views/Texts.vue'
+import Texts from '../views/texts/Texts.vue'
+import CreateText from '../views/texts/CreateText.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'texts',
     component: Texts
+  },
+  {
+    path: '/texts/new',
+    name: 'createText',
+    component: CreateText
   },
   {
     path: '/semantic',
---
+++ src/views/texts/CreateText.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form action="">
+      <input type="text"
+       v-model="title"
+       placeholder="title">
+      <button v-on:click="createText">
+        登録
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    createText() {
+      axios.post('/api/v1/texts/', {
+        title: this.title,
+      }).then(this.$router.push('/'))
+    }
+  }
+}
+</script>

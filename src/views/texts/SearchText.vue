2131c52081ef575ad5f839a81c6592b94ca17809
nargok <nargok@gmail.com>
2020-01-30 02:12:20 +0900

    display search result

--- src/views/texts/SearchText.vue
+++ src/views/texts/SearchText.vue
@@ -1,10 +1,38 @@
 <template>
   <div>
-    <input
-      type="text"
-      v-model="title"
-      placeholder="タイトル">
-    <button @click="searchBooks">検索</button>
+    <div>
+      <input
+        type="text"
+        v-model="title"
+        placeholder="タイトル">
+      <button @click="searchBooks">検索</button>
+    </div>
+    <div>
+      <ul v-for="(b, i) in this.books" :key="i">
+        <li class="book__list">
+          <table>
+            <tr>
+              <th>タイトル</th>
+              <td>{{b.volumeInfo.title}}</td>
+            </tr>
+            <tr>
+              <th>出版者</th>
+              <td>{{b.volumeInfo.publisher}}</td>
+            </tr>
+            <tr>
+              <th>出版日</th>
+              <td>{{b.volumeInfo.publishedDate}}</td>
+            </tr>
+            <tr>
+              <th></th>
+              <td>
+                <img :src="b.volumeInfo.imageLinks.smallThumbnail">
+              </td>
+            </tr>
+          </table>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,13 +43,21 @@ export default {
   data() {
     return {
       title: '',
+      books: [],
     }
   },
   methods: {
     searchBooks() {
       axios.get('https://www.googleapis.com/books/v1/volumes?q=' + this.title)
-       .then(res => console.log(res.data.items))
+       .then(res => this.books = res.data.items)
+       .catch(e => console.log(e))
     }
   }
 }
 </script>
+
+<style scoped>
+.book__list {
+  list-style: none;
+}
+</style>

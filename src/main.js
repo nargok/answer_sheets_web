cbbc6cb20636b7e78d547c9fcb461e2819a5b3fd
nargok <nargok@gmail.com>
2020-02-09 16:10:23 +0900

    post parameter for create text in search text page

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
 
 Vue.config.productionTip = false
 Vue.use(SuiVue)
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/views/texts/SearchText.vue
+++ src/views/texts/SearchText.vue
@@ -11,7 +11,7 @@
       <ul v-for="(b, i) in this.books" :key="i">
         <li class="book__list">
           <SearchTextItem :book="b" />
-          <button @click="createText">テキストを登録する</button>
+          <button @click="createText(b)">テキストを登録する</button>
         </li>
       </ul>
     </div>
@@ -19,7 +19,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 import SearchTextItem from './SearchTextItem.vue'
 
 export default {
@@ -34,12 +33,22 @@ export default {
   },
   methods: {
     searchBooks() {
-      axios.get('https://www.googleapis.com/books/v1/volumes?q=' + this.title)
-       .then(res => this.books = res.data.items)
-       .catch(e => console.log(e))
+      this.$http.get('https://www.googleapis.com/books/v1/volumes?q=' + this.title)
+        .then(res => this.books = res.data.items)
+        .catch(e => console.error(e))
     },
-    createText() {
-      console.info('create text from search component')
+    createText(book) {
+      const { volumeInfo } = book
+      const {
+        title,
+        publisher,
+        publishedDate,
+        imageLinks,
+      } = volumeInfo;
+      const {
+        smallThumbnail,
+      } = imageLinks;
+      console.info(title, publisher, publishedDate, smallThumbnail)
     }
   }
 }
